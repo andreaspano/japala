3b3f791df0810706b23f4051b7572fc5893cebd0
andreaspano <andrea.spano@quantide.com>
2019-03-11 10:13:03 +0100

    added proper object size in .o. now it prints with Mb, Gb etc added q()function ... now it quits and restart. Restart is --quiet

--- R/japala.R
+++ R/japala.R
@@ -1,6 +1,6 @@
 #' @title rm(list = ls(all =  T))
 #' @description remove all objects included dotted ones
-rm <- function() {
+r <- function() {
   all_names <- base::ls(all.names  = T, env = globalenv())
   base::rm(list = all_names, envir = globalenv())
   cat ( '---  deleted ', length(all_names) , 'objects ---', '\n')
@@ -66,22 +66,38 @@ o <- function() {
   
   name  <- ls(all.names = TRUE, env = globalenv())
   object <- lapply(name, get, envir = env)
-  size <- vapply(object, function(x) as.numeric(object.size(x)), FUN.VALUE = numeric(1))
+  .size <- vapply(object, function(x) as.numeric(object.size(x)), FUN.VALUE = numeric(1))
+  size <-  vapply(object, object_size, FUN.VALUE = character(1))
   typeof <- vapply(object, typeof, FUN.VALUE = character(1))
   class  <- vapply(object, function(x) paste(class(x), collapse = ' - '), FUN.VALUE = character(1))
 
-  # return
-  data.frame(name, size , class , typeof, stringsAsFactors = FALSE)
+  # out
+  out <-  data.frame(name, size , class , typeof, stringsAsFactors = FALSE)
+  out[order(.size, decreasing = T),]
+
 }  
-# --------------------------------------------------------------- 
-#' restart R session
-#' @export 
-#r <-  function() { system("R"); quit() }
 # ----------------------------------------------------------------
+#' @title object size
+#' @descriptoion returns object.size(x) formatted with the appropriate  unit of measurement.
+#' @export
+object_size <- function(x)  {
+  object_size <- object.size(x)
+  object_size
+  size <-  2^c(0, 10, 20,  30 ,  40)            
+  unit <-  c('byte', 'Kb', 'Mb', 'Gb', 'Tb') 
+  what <- max(seq_len(5)[(object_size/size) > 1])
+  paste(round((object_size / size[what]),2) , unit[what])
+}
+# ----------------------------------------------------------------
+#' @title Fresh start  
+#' @description Remove all objects and restart R session. All loaded packages will be unloaded
+q <- function() { 
+  japala:::r()
+  cat('---  Restarting R session ---', '\n')
+  system("R --quiet ") 
+  base::q("no") 
 
-
-
-
+}
 
 
 
@@ -94,9 +110,9 @@ o <- function() {
   makeActiveBinding(".dd", dd, env = ns)
   makeActiveBinding(".di", di, env = ns)
   makeActiveBinding(".o", o, env = ns)
-  makeActiveBinding(".rm", rm, env = ns)
+  makeActiveBinding(".r", r, env = ns)
 
-  namespaceExport(ns, c( '.p', '.q', '.dd', '.dl', '.di', '.o',  '.rm'))
+  namespaceExport(ns, c( '.p', '.q', '.dd', '.dl', '.di', '.o',  '.r'))
 }
 
 andrea_quantide <-  utils::person(given =  'Andrea', 
